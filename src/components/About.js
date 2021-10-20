7a98c64a28aec1282219b35e9a20d6779e907c18
andreas <andreas.widjaya@codecampz.com>
2021-10-20 15:54:49 +0700

    a-href vs link-to

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
+import { Link as L } from "react-router-dom";
+
 const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>      
-      <a href='/'>Go Back</a>
+      {/* <a href='/'>Go Back</a> */}
+      <L to='/'>Go Back</L>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
+import { Link as Lf } from "react-router-dom";
+
 const Footer = () => {
   return (
     <footer>
       <p>Copyright &copy; 2021</p>
-      <a href="/about">About</a>
+      {/* <a href="/about">About</a> */}
+      <Lf to='/about'>About</Lf>
     </footer>
   );
 }
