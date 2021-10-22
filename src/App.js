4c5f2e019b4225d79d9d75f8423dc34a014529e6
andreas <andreas.widjaya@codecampz.com>
2021-10-22 14:31:01 +0700

    using HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
